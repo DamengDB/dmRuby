f3ae32b567c21664c160c88bff4de4e13d2aea81
dm-lj <liujin@dameng.com>
2025-11-14 17:13:59 +0800

    上版本修改有误
    修改错误

--- ext/statement.c
+++ ext/statement.c
@@ -457,12 +457,21 @@ static VALUE rb_dm_stmt_execute(int argc, VALUE *argv, VALUE self) {
             rb_raise(cdmError, "failed to bind param %d", i);
           break;
         case T_FIXNUM:
+        #if SIZEOF_INT < SIZEOF_LONG
+          bind_buffers[i].buffer = xmalloc(sizeof(sdint8));
+          *(sdint8*)(bind_buffers[i].buffer) =(sdint8)FIX2LONG(argv[i]);
+          rt = dpi_bind_param(stmt_wrapper->stmt, i + 1, DSQL_PARAM_INPUT, DSQL_C_SBIGINT, stmt_wrapper->paramdesc[i].sql_type, stmt_wrapper->paramdesc[i].prec, stmt_wrapper->paramdesc[i].scale, bind_buffers[i].buffer, sizeof(sdint8), NULL);
+          if(!DSQL_SUCCEEDED(rt))
+            rb_raise(cdmError, "failed to bind param %d", i);
+          break;
+        #else
           bind_buffers[i].buffer = xmalloc(sizeof(sdint8));
           *(sdint8*)(bind_buffers[i].buffer) =(sdint8)FIX2INT(argv[i]);
           rt = dpi_bind_param(stmt_wrapper->stmt, i + 1, DSQL_PARAM_INPUT, DSQL_C_SBIGINT, stmt_wrapper->paramdesc[i].sql_type, stmt_wrapper->paramdesc[i].prec, stmt_wrapper->paramdesc[i].scale, bind_buffers[i].buffer, sizeof(sdint8), NULL);
           if(!DSQL_SUCCEEDED(rt))
             rb_raise(cdmError, "failed to bind param %d", i);
           break;
+        #endif
         case T_BIGNUM:
           {
             sdint8 num;
